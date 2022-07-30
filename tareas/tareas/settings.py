60d64e382023244eb03d8865d97d1bc0184868d5
Joaquin Valderrama <joaquin.valderrama.e@gmail.com>
2022-07-29 19:16:48 -0500

    second deploy

--- tareas/tareas/settings.py
+++ tareas/tareas/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from os import environ
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +29,7 @@ SECRET_KEY = 'django-insecure-^rz#%bl)!t*$xe^pfewf8n#m=$j4gda$kuf)tre6r-(3a=)c&9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- tareas/tareas/urls.py
+++ tareas/tareas/urls.py
@@ -1,10 +1,12 @@
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path,include
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from django.conf.urls.static import static
+from django.conf import settings
 schema_view = get_schema_view(
    openapi.Info(
       title="Aplicacion de gestion de tareas",
@@ -24,4 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('gestion/',include('gestion.urls')),
     path('autorizacion/',include('autorizacion.urls'))
-]
+] + static(settings.STATIC_URL,document_root= settings.STATIC_ROOT)
+appname='mainapp'
