00fc27182aa6a3ffc86723e6f5d3f8ef64ae6713
Joaquin Valderrama <joaquin.valderrama.e@gmail.com>
2022-07-30 20:51:51 -0500

    uso de los controladores

--- tareas/gestion/views.py
+++ tareas/gestion/views.py
@@ -3,9 +3,12 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.generics import ListAPIView,ListCreateAPIView
+
+# from tareas.autorizacion.models import Usuario
 from .serializer import pruebaSerializer,TareaSerializer
 from rest_framework import status
 from .models import Tarea
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 @api_view (http_method_names=['GET','POST'])
@@ -34,11 +37,12 @@ class ListarTareas(ListAPIView):
 class TareasView(ListCreateAPIView):
     queryset  = Tarea.objects.all() # Select *from Tarea
     serializer_class = TareaSerializer
-
-    def get(self, request):
+    permission_classes = [IsAuthenticated]
+    def get(self, request:Request):
         #Cuando se modifica el metodo por algun comportamiento diferente DRF ahora obedecera a este comportamiento, ahi se podra dejar de utilizar los atributos queryset y serializer_class
         #primero obtengo las tareas
-        tareas = self.get_queryset()
+        UsuarioId=request.user.id
+        tareas = Tarea.objects.filter(UsuarioId = UsuarioId).all()
         #luego la paso al serializador para convertirlas a tipos de datos genericos
         tareaSerializada = self.serializer_class(tareas, many=True)
         return Response (data={
@@ -49,6 +53,9 @@ class TareasView(ListCreateAPIView):
 
     def post(self,request:Request):
         body = request.data
+        # request.user => devolvera toda la instancia del usuario que esta en la token (basandose en su ID)si no hay un usuario entonces sera un AnonymousUser
+        print(request.user)
+        body['UsuarioId']  = request.user.id # modifico el body entrante y le agrego el ID del usuario que actualmente esta haciendo la peticion
         instancia_Serializador = self.serializer_class(data=body)
         validacion =  instancia_Serializador.is_valid(raise_exception=True) #me retornara true si es valida, sino es valida emitira un error
         
